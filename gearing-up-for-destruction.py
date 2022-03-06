53cdc06cd599730874d4ff51530b102a8d768d05
lsmcdon1 <lsmcdon1@asu.edu>
2022-03-05 21:50:03 -0500

    Added comments

--- gearing-up-for-destruction.py
+++ gearing-up-for-destruction.py
@@ -1,7 +1,7 @@
-import numpy as np
+import numpy as np 
 from fractions import Fraction
 
-def check(soln): 
+def check(soln): # Used to check if LinAlg produced a valid result (all postive vals greater than 1)
     for n in soln: 
         if n < 1.0: 
             return False
@@ -9,15 +9,22 @@ def check(soln):
     return True
 
 def solution(p):
-    l = len(p)
-    d = [0] * l
+    l = len(p) #number of pegs in use
+    d = [0] * l #list to track distance between pegs
     
     for n in range(0, l-1): 
-        d[n] = p[n+1] - p[n]
+        d[n] = p[n+1] - p[n] #calculates the distance between pegs
         
-    A = []
+    #init blank matrices for system of equations  
+    A = [] 
     b = []
     
+    # We need to make a system of equations that we can solve to determine the radius of each gear
+    # So, if there are three gears with radaii x, y, z, the equations would look as follows: 
+        # x + y = d0
+        # y + z = d1
+        # -x + 2z = 0  // this equation is used to bind via the overall gear ratio
+
     for n in range(0, l-1): 
         temp = [0] * l
         temp[n] = 1.0
@@ -32,14 +39,12 @@ def solution(p):
     A.append(temp)
     b.append(0.0)
 
-
-    
+    #use Numpy linalg to solve the system of equations
     soln = np.linalg.solve(A, b)
-    print(check(soln))
+    
+    #check the solution, format result as a fraction
     if check(soln): 
         frack = Fraction(soln[0]).limit_denominator()
         return frack.numerator, frack.denominator
     else: 
-        return [-1, -1]
-
-print(solution([4, 30, 50]))
+        return [-1, -1] #default return if non-valid solution found
--- ion-flux-relabeling.py
+++ ion-flux-relabeling.py
@@ -1,75 +1,70 @@
+#need a node class to build and iterate over the binary tree 
 class node:
     def __init__(self):
-        self.lc = -1
-        self.rc = -1
-        self.p = -1
-        self.l = -1
+        self.lc = -1 #left child node
+        self.rc = -1 #right child node
+        self.p = -1  #parent node
+        self.l = -1  #label, or numbered value
 
-def build_tree(h):
+def build_tree(h): #driver function to begin recursive build
     node_count = pow(2, h) - 1
     root_node = build_rec(h, node_count, 0, None)
 
     return root_node
 
-def build_rec(max_h, num, cur_h, parent):
-    temp = node()
-    temp.p = parent
-    temp.l = num
+def build_rec(max_h, cur_h, parent): #recursive build function 
+    temp = node() #generates a node per call
+    temp.p = parent #keeps track of parent from previous call
     if (cur_h < max_h - 1):
-        temp.lc = build_rec(max_h, int(num/2), cur_h + 1, temp)
-        temp.rc = build_rec(max_h, int(num-1), cur_h + 1, temp)
+        temp.lc = build_rec(max_h, cur_h + 1, temp) #recurse left children
+        temp.rc = build_rec(max_h, cur_h + 1, temp) #recurse right children
         return temp
     
-    if cur_h == max_h - 1: 
+    if cur_h == max_h - 1: #end condition, height reached
         return temp
 
-
-def number_tree(root):
+def number_tree(root): #driver function to number tree by post order traversal
     root = number_rec(root, 1)
 
     return root
 
-def number_rec(node, val):
-    if node.lc == -1:
+def number_rec(node, val): #recursive function to number nodes 
+    if node.lc == -1: #if no child nodes, take passed value 
         node.l = val
 
-    elif node.lc != -1:
-        node.lc = number_rec(node.lc, val)
-        node.rc = number_rec(node.rc, node.lc.l + 1)
-        node.l = node.rc.l + 1
+    elif node.lc != -1: #if child nodes remain, continue recursion 
+        node.lc = number_rec(node.lc, val) #left sub-tree takes priority via POT
+        node.rc = number_rec(node.rc, node.lc.l + 1) #right nodes follow 
+        node.l = node.rc.l + 1 #current recursed node takes right child val + 1
 
     return node
 
-def find_parent(root, c):
-    iter = root
+def find_parent(root, c): #function to traverse labeled tree to find parent 
+    iter = root #iteration node 
     found = False
 
-    if root.l == c: 
+    if root.l == c: #edge case: root node (no child)
         return -1
 
     while not found: 
-        if iter.l == c:
+        if iter.l == c: #if iter on child node, return parent label
             return iter.p.l
 
-        elif iter.lc.l != -1: 
+        elif iter.lc.l != -1: #if child nodes remain, traverse down the tree 
             if c <= iter.lc.l:
                 iter = iter.lc
             else: iter = iter.rc
 
+        else: #no child nodes, must be parent node via perfect tree 
+            return iter.l 
 
-        else: 
-            return iter.l
-
-
-def solution(h, query):
+def solution(h, query): #solution driver 
     root = build_tree(h)
     root = number_tree(root)
 
     results = []
 
-    for q in query: 
+    for q in query: #iterate through requested nodes
         results.append(find_parent(root, q))
 
     return results
-
-print(solution(5, [19, 14, 28]))
